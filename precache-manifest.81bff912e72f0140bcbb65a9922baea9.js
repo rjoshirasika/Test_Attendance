11eeb0405f9603421b4067933a7260872b82cef0
Rasika Joshi <rasika_j@tekditechnologies.com>
2022-03-07 19:29:19 +0530

    Updates

---
+++ precache-manifest.81bff912e72f0140bcbb65a9922baea9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2200bb77de80cb9ef33fddd040dbe84",
+    "url": "/Test_Attendance/index.html"
+  },
+  {
+    "revision": "ceb03ecf98d9a46ad493",
+    "url": "/Test_Attendance/static/css/main.71aebd05.chunk.css"
+  },
+  {
+    "revision": "65f876561642ab5b6adf",
+    "url": "/Test_Attendance/static/js/2.bf850a05.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Test_Attendance/static/js/2.bf850a05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceb03ecf98d9a46ad493",
+    "url": "/Test_Attendance/static/js/main.710ae2d1.chunk.js"
+  },
+  {
+    "revision": "39af57ac8e0859050a2f",
+    "url": "/Test_Attendance/static/js/runtime-main.0e147de3.js"
+  }
+]);
